46629fb2589ce447bc80a24cdfa7a6e0ff5d66e6
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-21 00:02:23 +0300

    this commit adds tests for the delete spec

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -47,4 +47,24 @@ describe("#update") do
       expect(stylist.name()).to(eq("Jane"))
     end
   end
+  describe("#delete") do
+    it("lets you delete a stylist from the database") do
+      stylist = Stylist.new("Jane", nil)
+      stylist.save()
+      stylist2 = Stylist.new("jill", nil)
+      stylist2.save()
+      stylist.delete()
+      expect(Stylist.all()).to(eq([stylist2]))
+    end
+    it("deletes a stylist's tasks from the database") do
+     stylist = Stylist.new("Jane", nil)
+     stylist.save()
+     client = Client.new("jack", stylist.id())
+     client.save()
+     client2 = Client.new("john", stylist.id())
+     client2.save()
+     stylist.delete()
+    expect(Client.all()).to(eq([]))
+   end
+  end
 end
