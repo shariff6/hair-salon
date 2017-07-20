3278c785c7e7929a2deaefce3e087584719e0ac2
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-20 23:43:00 +0300

    this commit adds a delete method to the stylist class'

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -22,19 +22,26 @@ class Stylist
 
   define_method(:==) do |another_stylist|
   self.name().==(another_stylist.name()).&(self.id().==(another_stylist.id()))
-end
-define_singleton_method(:find) do |id|
- found_stylist = nil
- Stylist.all().each() do |stylist|
-  if stylist.id().==(id)
-    found_stylist = stylist
   end
-end
-found_stylist
-end
-define_method(:update) do |name|
+
+  define_singleton_method(:find) do |id|
+   found_stylist = nil
+   Stylist.all().each() do |stylist|
+    if stylist.id().==(id)
+    found_stylist = stylist
+     end
+   end
+  found_stylist
+ end
+ define_method(:update) do |name|
   @name = name
   @id = self.id()
   DB.exec("UPDATE stylists SET name = '#{@name}' WHERE id = #{@id};")
+ end
+
+ define_method(:delete) do
+   DB.exec("DELETE FROM stylists WHERE id = #{self.id()};")
+   DB.exec("DELETE FROM clients WHERE list_id = #{self.id()};")
+ end
 end
 end
