7ef7dbe574491f50919f410b6a913ccecd81816a
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-20 22:38:29 +0300

    this commit adds the test for the find method

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -18,16 +18,25 @@ describe(Stylist) do
   end
 describe("#save") do
   it("lets you save stylists to the database") do
-    list = Stylist.new("Jane", nil)
-    list.save()
+    stylist = Stylist.new("Jane", nil)
+    stylist.save()
     expect(Stylist.all()).to(eq([list]))
   end
 end
 describe("#==") do
   it("proves it is the same stylist if they have the same name") do
-    list1 = Stylist.new("Jane",nil)
-    list2 = Stylist.new("Jane", nil)
-    expect(list1).to(eq(list2))
+    stylist1 = Stylist.new("Jane",nil)
+    stylist2 = Stylist.new("Jane", nil)
+    expect(stylist1).to(eq(stylist2))
   end
+end
+describe(".find") do
+it("returns a Stylist by its ID") do
+  test_stylist = Stylist.new("Jane", nil)
+  test_stylist.save()
+  test_stylist2 = Stylist.new("Jack",nil)
+  test_stylist2.save()
+  expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+end
 end
 end
