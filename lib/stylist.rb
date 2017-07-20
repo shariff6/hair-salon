b806010bd5b0a8fcb6408dc980ebe15e38876ae7
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-20 22:35:48 +0300

    this commit adds the find method to the list class

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -23,4 +23,13 @@ class Stylist
   define_method(:==) do |another_stylist|
   self.name().==(another_stylist.name()).&(self.id().==(another_stylist.id()))
 end
+define_singleton_method(:find) do |id|
+ found_stylist = nil
+ Stylist.all().each() do |stylist|
+  if stylist.id().==(id)
+    found_stylist = stylist
+  end
+end
+found_stylist
+end
 end
